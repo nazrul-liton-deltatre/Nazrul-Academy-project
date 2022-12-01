9cc13eda275f75b29e2de97dec200f7f8fdfdbc2
Nazrul-Liton-deltatre <nazrul.liton@deltatre.com>
2022-12-01 07:54:46 +0000

    homepage refactored

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,52 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React, { useContext } from "react";
+import postureImage from "../assets/classicHorror.jpeg";
+import SpatialNavigation, { Focusable } from "react-js-spatial-navigation";
+import { AssetContext } from "../AssetContext";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+	const navigateTo = useNavigate();
+	const { setAssetToViewObject } = useContext(AssetContext);
+
+	const watchVideo = (objectId) => {
+		setAssetToViewObject(objectId);
+		const urlToNavigateTo = `/video` + objectId;
+		navigateTo(urlToNavigateTo);
+	};
 	return (
 		<>
-			<Navbar bg="dark" expand="lg">
-				<Container>
-					<Navbar.Brand
-						style={{
-							color: "white",
-							fontWeight: 700,
-							letterSpacing: "1.5px",
-							fontSize: "34px",
-						}}
-						href="#home"
-					>
-						Netflix
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="justify-content-end">
-							<Nav.Link style={{ color: "white", fontWeight: 800 }} href="/">
-								Home
-							</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
+			<SpatialNavigation>
+				<div className="hero-image-block">
+					<img src={postureImage} alt="" srcSet="" />
+					<div className="nav-block">
+						<ul>
+							<li style={{ color: "red" }}>Movieflix</li>
+							<Focusable onClickEnter={() => navigateTo("/")}>
+								<a href="/">
+									<li>home</li>
+								</a>
+							</Focusable>
+							<Focusable onClickEnter={() => navigateTo("/")}>
+								<a href="/">
+									<li>most trending</li>
+								</a>
+							</Focusable>
+						</ul>
+					</div>
+					<div className="hero-asset-info">
+						<h1>A Classic Horror Story</h1>
+						<span>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Obcaecati laudantium saepe temporibus earum nemo dolorum
+							blanditiis veniam, error officia quam cupiditate, praesentium,
+							architecto tempora in molestias quo hic nulla quos?
+						</span>
+						<Focusable onClickEnter={() => watchVideo(1)}>
+							<button>Watch Now</button>
+						</Focusable>
+					</div>
+				</div>
+			</SpatialNavigation>
 		</>
 	);
 };
